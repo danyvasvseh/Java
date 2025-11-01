49d6cefde093371245e8681875658b98bdaa762c
Басс Иван Павлович <vanabassz@gmail.com>
2025-11-01 19:15:48 +0300

    Add files via upload

---
+++ b.java
@@ -0,0 +1,11 @@
+public class b {
+    public static void main(String[] args) {
+        while ( true) {
+            do {
+                System.out.println("ха");
+                break;
+            } while (true);
+            continue;
+        }
+    }
+}
---
+++ c.java
@@ -0,0 +1,11 @@
+public class c {
+    public static void main(String[] args) {
+
+        while ( true) {
+            do {
+                System.out.println("ха");
+            } while (true);
+            
+        }
+    }
+}
